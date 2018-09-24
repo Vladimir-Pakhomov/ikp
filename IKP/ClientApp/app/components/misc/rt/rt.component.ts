8cbb62c814b4c849e7510f48ce13da9b02cb3ecf
pahomov <va.pakhomov91@gmail.com>
2018-09-24 21:06:12 +0300

    Release-1.6 Переделал rt.component для отображения в виде двухстолбцового бланка с последовательным выбором.

--- IKP/ClientApp/app/components/misc/rt/rt.component.ts
+++ IKP/ClientApp/app/components/misc/rt/rt.component.ts
@@ -17,19 +17,56 @@ export class RtComponent implements OnInit {
 
     @Output() onItemSelected: EventEmitter<any> = new EventEmitter<any>();
 
+    hblocks: HBlock[] = [];
+
+    currentIndex: number = 0;
+
     constructor() { }
 
+    private recursion(level: any[]) {
+        for(let i=0; i<level.length; i++){
+            if(level[i][this.childrenProperty].length)
+            {
+                var values = [];
+                for(let j=0; j<level[i][this.childrenProperty].length; j++){
+                    if(level[i][this.childrenProperty][j][this.childrenProperty].length == 0)
+                        values.push(level[i][this.childrenProperty][j]);
+                    else
+                        this.recursion(level[i][this.childrenProperty]);
+                }
+                if(values.length > 0) {
+                    this.hblocks.push({
+                        key: level[i][this.displayProperty],
+                        values: values,
+                        levelIndex: this.hblocks.length
+                    });
+                }
+            }
+        }
+    }
+
     ngOnInit() {
-        
+        this.recursion(this.level);
     }
 
     itemSelected(item: any){
+        if(!item.isSuccess && item[this.correctnessProperty])
+            this.currentIndex++;
         this.onItemSelected.next(item);
     }
 
-    isDisplayNotNeeded(item: any): boolean{
-        // Display not needed if item is incorrect and all its correct brothers are already succeeded
-        return !item[this.correctnessProperty] &&
-         this.level.filter((x: any) => !!x[this.correctnessProperty] && !x.isSuccess).length == 0;
+    isBlockSuccess(block: HBlock) {
+        return block.values.filter((x: any) => !!x[this.correctnessProperty] && !x.isSuccess).length == 0;
     }
+
+    isDisplayNotNeeded(hblock: HBlock, item: any): boolean{
+        // Display not needed if item is incorrect and all its correct brothers are already succeeded
+        return !item[this.correctnessProperty] && hblock.values.filter(x => !!x[this.correctnessProperty] && !x.isSuccess).length == 0;
+    }
+}
+
+class HBlock {
+    key: string;
+    values: any[];
+    levelIndex: number;
 }
